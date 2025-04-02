044018b536c7970ef0cd35f3c5efa4c213ee20fc
Mahi Jain <mahij.2912@gmail.com>
2025-04-02 13:02:47 +0530

    database and backend basic connection

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+const connectDB = require("./database");
+
+const PORT = 5000;
 const app = express();
 
+connectDB();
 app.get("/", (req, res) => {
   res.send(`<h1>Hello Backend is working</h1>`);
 });
 
-const PORT = 5000;
+app.get("/hello", (req, res) => {
+  res.json({ message: "Hello, welcome!!!!" });
+});
 
 app.listen(PORT, () => {
   console.log(`server is running at port ${PORT}`);
