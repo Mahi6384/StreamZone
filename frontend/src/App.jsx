5ee51729da494e68167b522193ba5942d3487e7e
Mahi Jain <mahij.2912@gmail.com>
2026-04-07 11:21:53 +0530

    feat(app): register experience routes and legacy redirects

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,33 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter, Navigate, useParams } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import WatchPage from "./pages/WatchPage";
-import UploadPage from "./pages/UploadPage";
-import MyUploads from "./pages/MyUploads";
-import { Toaster } from "react-hot-toast";
+import ShareExperiencePage from "./pages/ShareExperiencePage";
+import MyExperiences from "./pages/MyExperiences";
+import ExperienceDetail from "./components/ExperienceDetail";
+import ThemedToaster from "./components/ThemedToaster";
 import Navbar from "./components/Navbar";
+
+function LegacyWatchRedirect() {
+  const { id } = useParams();
+  return <Navigate to={`/experience/${id}`} replace />;
+}
 
 function App() {
   return (
     <BrowserRouter>
-      <Toaster position="top-center" reverseOrder={false} />
+      <ThemedToaster />
       <Navbar />
       <Routes>
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Home />} />
-        <Route path="/upload" element={<UploadPage />} />
-        <Route path="/watch/:id" element={<WatchPage />} />
-        <Route path="/my-uploads" element={<MyUploads />} />
+        <Route path="/share" element={<ShareExperiencePage />} />
+        <Route path="/upload" element={<Navigate to="/share" replace />} />
+        <Route path="/experience/:id" element={<ExperienceDetail />} />
+        <Route path="/watch/:id" element={<LegacyWatchRedirect />} />
+        <Route path="/my-experiences" element={<MyExperiences />} />
+        <Route path="/my-uploads" element={<Navigate to="/my-experiences" replace />} />
       </Routes>
     </BrowserRouter>
   );
